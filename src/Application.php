b3ab39120ffbb499613f768a88b443e844988bc8
Dario Piedra <dpiedra@roccacr.com>
2021-09-10 10:14:45 -0600

    Segundo Commit

--- src/Application.php
+++ src/Application.php
@@ -58,13 +58,13 @@ class Application extends BaseApplication
             );
         }
 
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
-        if (Configure::read('debug')) {
-            $this->addPlugin('DebugKit');
-        }
+        // /*
+        //  * Only try to load DebugKit in development mode
+        //  * Debug Kit should not be installed on a production system
+        //  */
+        // if (Configure::read('debug')) {
+        //     $this->addPlugin('DebugKit');
+        // }
 
         // Load more plugins here
     }
